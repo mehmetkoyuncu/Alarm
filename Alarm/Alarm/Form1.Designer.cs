5b29642dbe0e3a0d90757c9ff403160330642e1e
Mehmet Koyuncu <46028783+mehmetkoyuncu@users.noreply.github.com>
2020-06-29 01:28:57 +0300

    Alarm Örnek

---
+++ Alarm/Alarm/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmbHour = new System.Windows.Forms.ComboBox();
+            this.cmbMinute = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAlarmKur = new System.Windows.Forms.Button();
+            this.lblClock = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbHour
+            // 
+            this.cmbHour.CausesValidation = false;
+            this.cmbHour.FormattingEnabled = true;
+            this.cmbHour.Location = new System.Drawing.Point(12, 50);
+            this.cmbHour.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbHour.Name = "cmbHour";
+            this.cmbHour.Size = new System.Drawing.Size(121, 26);
+            this.cmbHour.TabIndex = 0;
+            // 
+            // cmbMinute
+            // 
+            this.cmbMinute.FormattingEnabled = true;
+            this.cmbMinute.Location = new System.Drawing.Point(12, 114);
+            this.cmbMinute.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbMinute.Name = "cmbMinute";
+            this.cmbMinute.Size = new System.Drawing.Size(121, 26);
+            this.cmbMinute.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Saat :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dakika :";
+            // 
+            // btnAlarmKur
+            // 
+            this.btnAlarmKur.Location = new System.Drawing.Point(58, 148);
+            this.btnAlarmKur.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAlarmKur.Name = "btnAlarmKur";
+            this.btnAlarmKur.Size = new System.Drawing.Size(75, 32);
+            this.btnAlarmKur.TabIndex = 4;
+            this.btnAlarmKur.Text = "Alarm Kur";
+            this.btnAlarmKur.UseVisualStyleBackColor = true;
+            this.btnAlarmKur.Click += new System.EventHandler(this.btnAlarmKur_Click);
+            // 
+            // lblClock
+            // 
+            this.lblClock.AutoSize = true;
+            this.lblClock.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.lblClock.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblClock.Location = new System.Drawing.Point(222, 9);
+            this.lblClock.Name = "lblClock";
+            this.lblClock.Size = new System.Drawing.Size(38, 18);
+            this.lblClock.TabIndex = 5;
+            this.lblClock.Text = "label3";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(146, 50);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 130);
+            this.listBox1.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(163, 188);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(73, 23);
+            this.axWindowsMediaPlayer1.TabIndex = 7;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SpringGreen;
+            this.ClientSize = new System.Drawing.Size(272, 200);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblClock);
+            this.Controls.Add(this.btnAlarmKur);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbMinute);
+            this.Controls.Add(this.cmbHour);
+            this.Font = new System.Drawing.Font("Bahnschrift Condensed", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbHour;
+        private System.Windows.Forms.ComboBox cmbMinute;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAlarmKur;
+        private System.Windows.Forms.Label lblClock;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Alarm/Alarm/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alarm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                if (i.ToString().Length == 1)
+                {
+                    cmbMinute.Items.Add("0" + i.ToString());
+                }
+                else
+                {
+                    cmbMinute.Items.Add(i.ToString());
+                }
+
+            }
+
+            for (int i = 0; i < 24; i++)
+            {
+                if (i.ToString().Length == 1)
+                {
+                    cmbHour.Items.Add("0" + i.ToString());
+                }
+                else
+                {
+                    cmbHour.Items.Add(i.ToString());
+                }
+            }
+            timer2.Enabled = true;
+            timer2.Start();
+
+
+
+        }
+
+
+        private void btnAlarmKur_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cmbHour.Text != "" || cmbMinute.Text != "")
+                {
+                    if (int.Parse(cmbHour.Text) < 0 || int.Parse(cmbHour.Text) > 23)
+                    {
+                        MessageBox.Show("Geçersiz Saat");
+                    }
+                    else if (int.Parse(cmbMinute.Text) < 0 || int.Parse(cmbMinute.Text) > 59)
+                    {
+                        MessageBox.Show("Geçersiz Dakika");
+                    }
+
+                    else
+                    {
+                        listBox1.Items.Add("Alarm : " + cmbHour.Text.ToString() + " " + cmbMinute.Text.ToString());
+                        timer1.Enabled = true;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Saat veya Dakika Boş Geçilemez.."); ;
+                }
+
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Metin Girişi Yapılamaz..");
+
+            }
+
+
+
+
+
+
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string hour = DateTime.Now.Hour.ToString();
+            if (hour.ToString().Length == 1)
+            {
+                hour = "0" + hour;
+            }
+            string minute = DateTime.Now.Minute.ToString();
+            if (minute.ToString().Length == 1)
+            {
+                minute = "0" + minute;
+            }
+
+            lblClock.Text = hour + " : " + minute;
+
+
+            if (cmbMinute.Text == minute && cmbHour.Text == hour)
+            {
+                timer1.Enabled = false;
+
+                string path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sound\\sound1.mp3"));
+                axWindowsMediaPlayer1.URL = path;
+                MessageBox.Show("Alarm Saati !!");
+                axWindowsMediaPlayer1.URL = null;
+            }
+
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            string hour = DateTime.Now.Hour.ToString();
+            string minutE = DateTime.Now.Minute.ToString();
+            if (hour.ToString().Length == 1)
+            {
+                hour = "0" + hour;
+            }
+            if (minutE.ToString().Length == 1)
+            {
+                minutE = "0" + minutE;
+            }
+
+
+            lblClock.Text = hour + " : " + minutE;
+        }
+    }
+}
